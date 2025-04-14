caeb10f7351fbcd23e3c0618beb2b9b11fba46c1
SATHVIK SVS <svssathvik77@gmail.com>
2025-04-14 12:04:31 +0530

    add comments to contract

--- src/MultiCall.sol
+++ src/MultiCall.sol
@@ -2,17 +2,33 @@
 pragma solidity ^0.8.13;
 
 contract MultiCall {
+    /*
+     * @notice A single call to target a contract with a given callData
+     * @param target The contract to call
+     * @param callData The data to pass to the contract
+     * @param allowFailure Whether to allow the sub-call to fail
+     */
     struct Call {
         address target;
         bytes callData;
         bool allowFailure;
     }
 
+    /*
+     * @notice A single result from a call
+     * @param isSuccess Whether the call was successful
+     * @param returnData The return data from the call
+     */
     struct Result {
         bool isSuccess;
         bytes returnData;
     }
 
+    /*
+     * @notice Aggregate multiple calls into a single call
+     * @param calls The calls to aggregate
+     * @return blockNumber The block number of the call and returnData The return data of the calls
+     */
     function aggregate(
         Call[] calldata calls
     ) external returns (uint256 blockNumber, Result[] memory returnData) {
