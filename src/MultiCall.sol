4092fa92bfebdb40a5afbfadb869f0a181671711
SATHVIK SVS <svssathvik77@gmail.com>
2025-04-14 14:26:28 +0530

    import re-entrancy guard

--- src/MultiCall.sol
+++ src/MultiCall.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract MultiCall {
     mapping(address => bool) private owners;
